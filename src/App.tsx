4667954167f970e6e82cbc55dd25cb52b4583ca3
Karel <karelh2207@gmail.com>
2024-11-09 13:07:53 -0300

    updating

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import "./App.css";
-import MiniDrawer from "./components/sidebar";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { lightTheme, darkTheme } from "./themes";
 import { useEffect, useState } from "react";
-import ResponsiveAppBar from "./components/header";
+// import ResponsiveAppBar from "./components/header";
 import { BrowserRouter } from "react-router-dom";
+import Dashboard from "./pages/dashboard/Dashboard";
 
 export default function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
-  // Función para alternar el modo
-  const mode = (newMode: boolean) => {
-    setIsDarkMode(newMode);
-  };
-
+  
   // Auto-theme detection
   useEffect(() => {
     const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
@@ -38,8 +34,7 @@ export default function App() {
       <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
         <BrowserRouter>
           <CssBaseline />
-          <ResponsiveAppBar isDarkMode={isDarkMode} mode={mode} />
-          <MiniDrawer  />
+          <Dashboard />
         </BrowserRouter>
       </ThemeProvider>
     </>
